d78d63c96f176cc71c1229d7cdd5a1d446281aba
Abhinav Jindal <abhinavjindal26@users.noreply.github.com>
2026-05-13 19:59:20 +0530

    fix: update next image config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,16 @@ const nextConfig: NextConfig = {
     root: process.cwd(),
   },
   images: {
-    domains: ["images.unsplash.com", "images.pexels.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
   },
 };
 
